fbfc212d234ad6f7cd23cbf2bc81be70726b25f7
Graham Cox <graham@grahamcox.co.uk>
2013-10-21 20:37:20 +0100

    Servlet mounts now come from a map. This is key for what comes next

--- webapp/src/main/scala/ScalatraBootstrap.scala
+++ webapp/src/main/scala/ScalatraBootstrap.scala
@@ -1,14 +1,26 @@
 import org.scalatra._
 import javax.servlet.ServletContext
+import grizzled.slf4j.Logger
 
 /**
  * Bootstrap the Scalatra system
  */
 class ScalatraBootstrap extends LifeCycle {
+  private val logger = Logger[this.type]
+
   override def init(context: ServletContext) {
     val accessTokenService = new uk.co.grahamcox.books.oauth2.AccessTokenService
 
-    context.mount(new uk.co.grahamcox.books.controllers.debug.DebugServlet, "/debug")
-    context.mount(new uk.co.grahamcox.books.controllers.oauth2.OAuth2Servlet(accessTokenService), "/oauth/2.0")
+    val servlets = Map(
+      "/debug" -> new uk.co.grahamcox.books.controllers.debug.DebugServlet,
+      "/oauth/2.0" -> new uk.co.grahamcox.books.controllers.oauth2.OAuth2Servlet(accessTokenService)
+    )
+
+    servlets.foreach {
+      case (key, servlet) => {
+        logger.info(s"Mounting servlet $servlet on $key")
+        context.mount(servlet, key)
+      }
+    }
   }
 }
