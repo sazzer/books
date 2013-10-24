4ac52fa35fee3a9d65b23cd0b0df353d47fc4544
Graham Cox <graham@grahamcox.co.uk>
2013-10-24 12:54:46 +0100

    Enough to actually build YUI modules into the War correctly

--- webapp/build-yui3.sh
+++ webapp/build-yui3.sh
@@ -3,7 +3,7 @@
 pushd `dirname $0`
 BASE_DIR=`pwd -P`
 
-shifter --walk --recursive --build-dir $BASE_DIR/target/webapp/scripts/
-yogi loader --yes --start $BASE_DIR/src/main/scripts --json $BASE_DIR/target/webapp/scripts/loader.json --js $BASE_DIR/target/webapp/scripts/loader.js
+shifter --walk --recursive --build-dir $BASE_DIR/build/webapp/scripts/
+yogi loader --yes --start $BASE_DIR/src/main/scripts --json $BASE_DIR/build/webapp/scripts/loader.json --js $BASE_DIR/build/webapp/scripts/loader.js
 
 popd
