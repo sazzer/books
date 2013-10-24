dc198a4ffaf4e0e37b1e2546c33288c674cfe44c
Graham Cox <graham@grahamcox.co.uk>
2013-10-24 20:05:41 +0100

    Actually able to load YUI modules and make use of them

---
+++ webapp/src/main/scripts/application/js/application.js
@@ -0,0 +1,10 @@
+Y.namespace("Books").Application = Y.Base.create("application", Y.App, [
+  // Extensions
+], {
+  // Prototype
+  initializer: function() {
+    Y.log("Starting application");
+  }
+}, {
+  // Statics
+});
