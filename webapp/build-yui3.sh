ec6e3661d1d4a102ec35f02b6a41234bfd1fb1c4
Graham Cox <graham@grahamcox.co.uk>
2013-10-24 12:59:47 +0100

    Removed the load-tests file

--- webapp/build-yui3.sh
+++ webapp/build-yui3.sh
@@ -4,6 +4,6 @@ pushd `dirname $0`
 BASE_DIR=`pwd -P`
 
 shifter --walk --recursive --build-dir $BASE_DIR/build/webapp/scripts/
-yogi loader --yes --start $BASE_DIR/src/main/scripts --json $BASE_DIR/build/webapp/scripts/loader.json --js $BASE_DIR/build/webapp/scripts/loader.js
+yogi loader --yes --start $BASE_DIR/src/main/scripts --json $BASE_DIR/build/webapp/scripts/loader.json --js $BASE_DIR/build/webapp/scripts/loader.js --tests $BASE_DIR/build/load-tests.js
 
 popd
