651353728c1004b42b944e66bde3269c40d7760c
Graham Cox <graham@grahamcox.co.uk>
2013-11-02 00:24:00 +0000

    Easy mechanism to build an Authorization header

--- oauth/src/main/scala/uk/co/grahamcox/oauth/Authorization.scala
+++ oauth/src/main/scala/uk/co/grahamcox/oauth/Authorization.scala
@@ -16,7 +16,7 @@ class Authorization(val consumerKey: String,
   val nonce: Nonce, 
   val signature: Signature, 
   val timestamp: DateTime,
-  val token: String, 
+  val token: Option[String],
   val version: String) {
   /**
    * Build the actual Authorization string to use from the header values
@@ -28,13 +28,53 @@ class Authorization(val consumerKey: String,
       "oauth_signature" -> signature.value,
       "oauth_signature_method" -> signature.method,
       "oauth_timestamp" -> (timestamp.withZone(DateTimeZone.UTC).getMillis() / 1000).toString(),
-      "oauth_token" -> token,
       "oauth_version" -> version)
+    val tokenValues = token match {
+      case Some(t: String) => Map("oauth_token" -> t)
+      case None => Map.empty[String, String]
+    }
 
-    val headerList = headerValues.foldLeft(Seq.empty[String]) {
+    val headerList = (headerValues ++ tokenValues).foldLeft(Seq.empty[String]) {
       case (soFar, (key, value)) => soFar ++ Seq(PercentEncoder(key) + "=\"" + PercentEncoder(value) + "\"")
     } sorted
 
     headerList.mkString("OAuth ", ", ", "")
+  }
+}
+
+/**
+ * Companion object to take the pain out of building an Authorization header
+ */
+object Authorization {
+  /**
+   * Build an Authorization object for the given request
+   * @param request The request to authorize
+   * @param consumerKey the consumer key to use
+   * @param token The auth token to use
+   * @param nonce The nonce to use
+   * @param timestamp The timestamp to use
+   * @param signingMethod The signing method to use 
+   * @param version The OAuth version to use
+   */
+  def apply(request: Request, 
+    consumerKey: Key, 
+    token: Option[Key],
+    nonce: Nonce = new Nonce(),
+    timestamp: DateTime = DateTime.now,
+    signingMethod: SigningMethod = new HmacSha1SigningMethod(),
+    version: String = "1.0"): Authorization = {
+      val signature = Signature(request, 
+        consumerKey, 
+        token, 
+        nonce, 
+        timestamp, 
+        signingMethod,
+        version)
+      new Authorization(consumerKey.key, 
+        nonce, 
+        signature,
+        timestamp,
+        token.map { t => t.key },
+        version)
   }
 }
--- oauth/src/test/scala/uk/co/grahamcox/oauth/AuthorizationSpec.scala
+++ oauth/src/test/scala/uk/co/grahamcox/oauth/AuthorizationSpec.scala
@@ -6,14 +6,28 @@ import com.github.nscala_time.time.Imports._
 class AuthorizationSpec extends SpecificationWithJUnit {
   "Authorization" should {
     "return the correct string" in {
-      val auth = new Authorization("xvz1evFS4wEEPTGEFPHBog", 
-        new Nonce("kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg"),
-        new Signature("tnnArxj06cWHq44gCs1OSKk/jLY=", "HMAC-SHA1"),
-        new DateTime(1318622958000L),
-        "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb",
-        "1.0")
+      "from the actual class" in {
+        val auth = new Authorization("xvz1evFS4wEEPTGEFPHBog", 
+          new Nonce("kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg"),
+          new Signature("tnnArxj06cWHq44gCs1OSKk/jLY=", "HMAC-SHA1"),
+          new DateTime(1318622958000L),
+          Some("370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb"),
+          "1.0")
 
-      auth.toString must beEqualTo("OAuth oauth_consumer_key=\"xvz1evFS4wEEPTGEFPHBog\", oauth_nonce=\"kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg\", oauth_signature=\"tnnArxj06cWHq44gCs1OSKk%2FjLY%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1318622958\", oauth_token=\"370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb\", oauth_version=\"1.0\"")
+        auth.toString must beEqualTo("OAuth oauth_consumer_key=\"xvz1evFS4wEEPTGEFPHBog\", oauth_nonce=\"kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg\", oauth_signature=\"tnnArxj06cWHq44gCs1OSKk%2FjLY%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1318622958\", oauth_token=\"370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb\", oauth_version=\"1.0\"")
+      }
+      "from the helper object" in {
+        val auth = Authorization(new Request("POST", "https://api.twitter.com/1/statuses/update.json", Map(
+            "include_entities" -> "true",
+            "status" -> "Hello Ladies + Gentlemen, a signed OAuth request!"
+          )), 
+          new Key("xvz1evFS4wEEPTGEFPHBog", "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw"),
+          Some(new Key("370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb", "LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE")),
+          new Nonce("kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg"),
+          new DateTime(1318622958000L))
+
+        auth.toString must beEqualTo("OAuth oauth_consumer_key=\"xvz1evFS4wEEPTGEFPHBog\", oauth_nonce=\"kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg\", oauth_signature=\"tnnArxj06cWHq44gCs1OSKk%2FjLY%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1318622958\", oauth_token=\"370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb\", oauth_version=\"1.0\"")
+      }
     }
   }
 }
